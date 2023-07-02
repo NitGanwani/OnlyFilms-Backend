e142f53ffca3bcb4e46bb84d97ea82ddf489265d
Nitin Ganwani <nitganwani@gmail.com>
2023-07-02 13:48:09 +0200

    Add interceptor and film controller

---
+++ src/middleware/auth.interceptor.ts
@@ -0,0 +1,64 @@
+import createDebug from 'debug';
+import { FilmRepo } from '../repository/film.m.repository';
+import { Request, Response, NextFunction } from 'express';
+import { HttpError } from '../types/http.error';
+import { AuthServices, PayloadToken } from '../services/auth';
+
+const debug = createDebug('FP:AuthInterceptor');
+
+export class AuthInterceptor {
+  // eslint-disable-next-line no-unused-vars
+  constructor(private filmRepo: FilmRepo) {
+    debug('Instantiated');
+  }
+
+  logged(req: Request, res: Response, next: NextFunction) {
+    try {
+      const authHeader = req.get('Authorization');
+      if (!authHeader) {
+        throw new HttpError(401, 'Not Authorized', 'Not Authorization header');
+      }
+
+      if (!authHeader.startsWith('Bearer')) {
+        throw new HttpError(
+          401,
+          'Not Authorized',
+          'Not Bearer in Authorization header'
+        );
+      }
+
+      const token = authHeader.slice(7);
+      const payload = AuthServices.verifyJWTGettingPayload(token);
+
+      req.body.tokenPayload = payload;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async authorizedForBooks(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.body.tokenPayload) {
+        throw new HttpError(
+          498,
+          'Token not found',
+          'Token not found in Authorized interceptor'
+        );
+      }
+
+      const { id: userID } = req.body.tokenPayload as PayloadToken;
+      const { id: filmId } = req.params;
+
+      const film = await this.filmRepo.queryById(filmId);
+
+      if (film.owner.id !== userID) {
+        throw new HttpError(401, 'Not authorized', 'Not authorized');
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
