1e52b1ff188c2e25da7c5a8c0fcbc6eb5e7c58d3
Nitin Ganwani <nitganwani@gmail.com>
2023-07-10 19:11:11 +0200

    Add firebase config files

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,11 @@ export const user = process.env.DB_USER;
 export const passwd = process.env.DB_PASSWORD;
 export const db = process.env.DB_NAME;
 export const secret = process.env.JWT_SECRET;
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'onlyfilms-3b5d5.firebaseapp.com',
+  projectId: 'onlyfilms-3b5d5',
+  storageBucket: 'onlyfilms-3b5d5.appspot.com',
+  messagingSenderId: '393689380288',
+  appId: '1:393689380288:web:94aaefdf480775bd864425',
+};
